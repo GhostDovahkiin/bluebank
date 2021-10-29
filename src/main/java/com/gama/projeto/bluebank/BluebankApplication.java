ad99be5c52fff9783ca5e408dc814d5007732bc7
GhostDovahkiin <pedro.sousa@dcx.ufpb.br>
2021-10-29 16:33:54 -0300

    refs #0001 - Estrutura inicial do projeto

---
+++ src/main/java/com/gama/projeto/bluebank/BluebankApplication.java
@@ -0,0 +1,13 @@
+package com.gama.projeto.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluebankApplication.class, args);
+	}
+
+}
