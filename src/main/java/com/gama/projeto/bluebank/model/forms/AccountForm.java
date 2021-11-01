8cc3ad14f5d13af3d733935d81faefe586ec142a
Murilo F. Saidel <51511259+muriloFsaidel@users.noreply.github.com>
2021-10-31 21:48:06 -0300

    Correção

--- src/main/java/com/gama/projeto/bluebank/model/forms/AccountForm.java
+++ src/main/java/com/gama/projeto/bluebank/model/forms/AccountForm.java
@@ -1,9 +1,17 @@
 package br.com.gamastore.bluebank.forms;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class AccountForm {
 
+	@NotNull
+	@Size(min=4, max =4)
 	public Integer agency;
+	@NotNull
+	@Size(min=4, max=10)
 	public Integer number;
+	@NotNull
 	public double amount;
 	
 }
