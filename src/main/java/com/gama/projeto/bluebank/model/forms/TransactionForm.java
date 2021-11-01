f8171d1da341363c2f9d427dc9dec8654f85a4bf
Murilo F. Saidel <51511259+muriloFsaidel@users.noreply.github.com>
2021-10-31 21:51:18 -0300

    correção

--- src/main/java/com/gama/projeto/bluebank/model/forms/TransactionForm.java
+++ src/main/java/com/gama/projeto/bluebank/model/forms/TransactionForm.java
@@ -1,8 +1,10 @@
 package br.com.gamastore.bluebank.forms;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
 
 public class TransactionForm {
-
+	
+	@NotNull
 	public LocalDateTime date;
 }
